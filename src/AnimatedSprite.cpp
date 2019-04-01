a607ee6985c0805e0f1555015f6bc131ee57ef41
sarahkittyy <chrisohlin1@gmail.com>
2019-04-01 12:24:38 -0400

    extra docs

--- src/AnimatedSprite.cpp
+++ src/AnimatedSprite.cpp
@@ -3,6 +3,7 @@
 AnimatedSprite::AnimatedSprite(sf::Texture& tex)
 	: mTex(tex)
 {
+	//Initialize default settings.
 	mRect.setTexture(&mTex, true);
 	mTexMapSize = {1, 1};
 	resetAnimation();
@@ -21,7 +22,7 @@ void AnimatedSprite::setTextureMapSize(sf::Vector2u size)
 }
 
 void AnimatedSprite::addAnimation(std::string name,
-								  AnimatedSprite::Animation animation)
+								  Animation animation)
 {
 	mAnimations[name] = animation;
 }
@@ -34,6 +35,7 @@ void AnimatedSprite::setAnimation(std::string name)
 
 void AnimatedSprite::update()
 {
+	//Attempt to find the current animation.
 	auto anim = mAnimations.find(mCurrentAnimation);
 	if (anim == mAnimations.end())
 	{
@@ -42,14 +44,25 @@ void AnimatedSprite::update()
 
 	Animation a = anim->second;
 
+	//If it's frozen, just return.
+	if (a.speed = -1)
+	{
+		return;
+	}
+
+	//If it's ready to update...
 	if (mAnimationClock.getElapsedTime() >= sf::seconds(a.speed))
 	{
+		//Restart the clock, increment the frame.
 		mAnimationClock.restart();
 		mCurrentFrame++;
+		//Loop the frame back to 0 if necessary.
 		if (mCurrentFrame >= a.frames.size())
 		{
 			mCurrentFrame = 0;
 		}
+
+		//Move the rect index over to the current frame.
 		setRectIndex(mCurrentFrame);
 	}
 }
@@ -68,21 +81,27 @@ void AnimatedSprite::resetAnimation()
 
 void AnimatedSprite::setRectIndex(size_t index)
 {
+	//Get the current frame from the index.
 	unsigned frame = mAnimations[mCurrentAnimation].frames[index];
 
+	//Get the texture x and y position.
 	size_t tx = frame % mTexMapSize.x;
 	size_t ty = frame / mTexMapSize.x;
 
 	// clang-format off
+	
+	//Get the actual sprite size.
 	sf::Vector2u tex_size{
 		mTex.getSize().x / mTexMapSize.x,
 		mTex.getSize().y / mTexMapSize.y
 	};
 	// clang-format on
 
+	//Get the x and y position inside the texture of the tile we want.
 	size_t px = tx * tex_size.x;
 	size_t py = ty * tex_size.y;
 
+	//Set the rect to focus on that position & size.
 	mRect.setTextureRect(sf::IntRect(px, py, tex_size.x, tex_size.y));
 	mRect.setSize({tex_size.x, tex_size.y});
 }
