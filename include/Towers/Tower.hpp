4431aab42595fb2a9017410a6c700e0c73bc1c4a
sarahkittyy <chrisohlin1@gmail.com>
2019-04-01 16:12:40 -0400

    added some interfaces for the tower's state

--- include/Towers/Tower.hpp
+++ include/Towers/Tower.hpp
@@ -10,14 +10,24 @@ namespace Towers
 class Tower : public sf::Drawable, public sf::Transformable
 {
 public:
-	Tower(sf::Texture& tex);
+	Tower(sf::Texture* tex);
 
 	virtual ~Tower();
+
+	void update();
+
+	void setTexture(sf::Texture* tex);
+
+	void addUpgrade(std::string name, AnimatedSprite::Animation anim);
+
+	void setUpgrade(std::string name);
+
+	void setTextureMapSize(sf::Vector2u size);
 
 private:
 	virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const;
 
-	sf::Texture& mTex;
+	sf::Texture* mTex;
 	AnimatedSprite mSprite;
 };
 
--- src/Towers/Tower.cpp
+++ src/Towers/Tower.cpp
@@ -3,14 +3,41 @@
 namespace Towers
 {
 
-Tower::Tower(sf::Texture& tex)
+Tower::Tower(sf::Texture* tex)
 	: mTex(tex),
 	  mSprite(mTex)
 {
 }
 
+
 Tower::~Tower()
 {
+}
+
+void Tower::update()
+{
+	mSprite.update();
+}
+
+void Tower::setTexture(sf::Texture* tex)
+{
+	mTex = tex;
+	mSprite.setTexture(mTex);
+}
+
+void Tower::addUpgrade(std::string name, AnimatedSprite::Animation anim)
+{
+	mSprite.addAnimation(name, anim);
+}
+
+void Tower::setUpgrade(std::string name)
+{
+	mSprite.setAnimation(name);
+}
+
+void Tower::setTextureMapSize(sf::Vector2u size)
+{
+	mSprite.setTextureMapSize(size);
 }
 
 void Tower::draw(sf::RenderTarget& target, sf::RenderStates states) const
