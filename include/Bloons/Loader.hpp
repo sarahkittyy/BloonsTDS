8968f62ce5f9a5d41548b14bcab3251f3ce7ff8d
sarahkittyy <chrisohlin1@gmail.com>
2019-04-02 14:31:26 -0400

    added BloonLoader

--- include/Bloons/Loader.hpp
+++ include/Bloons/Loader.hpp
@@ -11,7 +11,44 @@ namespace Bloons
 namespace Loader
 {
 
+/**
+ * @brief Initialze the given map from the given map config json file.
+ * 
+ * @param map The map to initialize.
+ * @param name The name of the config file.
+ */
 void loadMap(Map& map, std::string name);
+
+/**
+ * @brief Mostly static class to load bare copyable instances
+ * of bloons and their properties.
+ * 
+ */
+class BloonLoader
+{
+public:
+	/**
+	 * @brief Call this ONCE.
+	 * Initializes bloons from the default config file.
+	 * 
+	 */
+	static void init();
+
+	/**
+	 * @brief Get the bloon with the given name.
+	 * 
+	 * @param name The name of the bloon.
+	 * @return const Bloon& A const reference to the bloon.
+	 */
+	static const Bloon& getBloon(std::string name);
+
+private:
+	/**
+	 * @brief A map of names to their bloons.
+	 * 
+	 */
+	static std::unordered_map<std::string, Bloon> mBloons;
+};
 
 }
 }
--- src/Bloons/Loader.cpp
+++ src/Bloons/Loader.cpp
@@ -5,6 +5,59 @@ namespace Bloons
 namespace Loader
 {
 
+std::unordered_map<std::string, Bloon> BloonLoader::mBloons;
+
+void BloonLoader::init()
+{
+	using nlohmann::json;
+
+	//Load bloon config data.
+	std::ifstream file("resource/bloons/bloons.json");
+	if (!file)
+	{
+		throw std::runtime_error("Could not open bloon config file!");
+	}
+	json bloon_data;
+	file >> bloon_data;
+	file.close();
+
+	for (auto& bloon : bloon_data.at("bloons").items())
+	{
+		//Create the variables to emplace into the map later.
+		std::string name = bloon.key();
+		Bloon bl;
+
+		//Bloon data.
+		json data = bloon.value();
+
+		//Set the bloon speed.
+		bl.setSpeed(data.at("speed").get<float>());
+
+		//Iterate over all elements that the bloon pops to.
+		for (auto& pop_to : data.at("pops-to").get<json::array_t>())
+		{
+			//Append the item to the Bloon instance.
+			bl.addInside({
+				.name = pop_to.at("name"),
+				.ct = pop_to.at("count").get<unsigned>()
+			});
+		}
+
+		//Emplace the final bloon template into the map.
+		mBloons[name] = bl;
+	}
+}
+
+const Bloon& BloonLoader::getBloon(std::string name)
+{
+	if(mBloons.find(name) == mBloons.end())
+	{
+		throw std::out_of_range("Bloon " + name + " not found.");
+	}
+	
+	return mBloons[name];
+}
+
 void loadMap(Map& map, std::string name)
 {
 	using nlohmann::json;
@@ -44,7 +97,7 @@ void loadMap(Map& map, std::string name)
 		for (auto& group : wave.at("groups"))
 		{
 			//Load the bloon
-			Bloon b;   //TODO: LOAD BLOON
+			Bloon b = BloonLoader::getBloon(group.at("bloon").get<std::string>());
 			//Get the spacing.
 			float spacing = group.at("spacing").get<float>();
 			//Get the bloon count.
