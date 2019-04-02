84db5c9195497c1002736585cff34ff0f3f7f4ed
sarahkittyy <chrisohlin1@gmail.com>
2019-04-02 17:58:32 -0400

    implemented a basic resource manager

---
+++ include/ResourceManager.hpp
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <SFML/Graphics.hpp>
+
+#include <string>
+#include <unordered_map>
+
+/**
+ * @brief Manages textures so that only one of every texture is ever
+ * instanced at a time.
+ * 
+ */
+class ResourceManager
+{
+public:
+	/**
+	 * @brief Retrieve the texture at the given path.
+	 * 
+	 * @param path The path of the texture.
+	 * @return const sf::Texture& A const ref to the texture.
+	 */
+	const sf::Texture& texture(std::string path);
+
+private:
+	/**
+	 * @brief The internal map of textures.
+	 * 
+	 */
+	std::unordered_map<std::string, sf::Texture> mTextures;
+};
---
+++ src/ResourceManager.cpp
@@ -0,0 +1,16 @@
+#include "ResourceManager.hpp"
+
+const sf::Texture& ResourceManager::texture(std::string path)
+{
+	auto found = mTextures.find(path);
+	if (found == mTextures.end())
+	{
+		mTextures[path] = sf::Texture();
+		mTextures[path].loadFromFile(path);
+		return mTextures[path];
+	}
+	else
+	{
+		return found->second;
+	}
+}
