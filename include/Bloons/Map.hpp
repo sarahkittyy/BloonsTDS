7b1af87014e67b153ae5b6abdaa9feb75ce91712
sarahkittyy <chrisohlin1@gmail.com>
2019-04-02 13:33:12 -0400

    added some path/wave elements to the map

--- include/Bloons/Map.hpp
+++ include/Bloons/Map.hpp
@@ -1,5 +1,8 @@
 #pragma once
 
+#include <string>
+#include <vector>
+
 #include "Bloons/Path.hpp"
 #include "Bloons/Wave.hpp"
 
@@ -9,7 +12,19 @@ namespace Bloons
 class Map
 {
 public:
+	Map();
+
+	void addPath(Path path);
+
+	void addWave(Wave wave);
+
+	void runWave(size_t id);
+
+	void update();
+
 private:
+	std::vector<Path> mPaths;
+	std::vector<Wave> mWaves;
 };
 
 }
--- src/Bloons/Map.cpp
+++ src/Bloons/Map.cpp
@@ -3,6 +3,26 @@
 namespace Bloons
 {
 
+Map::Map()
+{
+}
 
+void Map::addPath(Path path)
+{
+	mPaths.push_back(path);
+}
+
+void Map::addWave(Wave wave)
+{
+	mWaves.push_back(wave);
+}
+
+void Map::runWave(size_t id)
+{
+}
+
+void Map::update()
+{
+}
 
 }
