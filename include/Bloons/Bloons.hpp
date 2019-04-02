35bae1be0936ca7e142ced7ec6fb9d11f251c711
sarahkittyy <chrisohlin1@gmail.com>
2019-04-02 18:45:47 -0400

    added includes

--- include/Bloons/Bloons.hpp
+++ include/Bloons/Bloons.hpp
@@ -2,8 +2,10 @@
 
 #include "Bloons/Bloon.hpp"
 #include "Bloons/Loader.hpp"
+#include "Bloons/Manager.hpp"
 #include "Bloons/Map.hpp"
 #include "Bloons/Path.hpp"
+#include "Bloons/Renderer.hpp"
 #include "Bloons/Wave.hpp"
 
 namespace Bloons
