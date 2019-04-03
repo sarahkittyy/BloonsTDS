87999734534ee086ffd688e285526f57edfa3d18
sarahkittyy <chrisohlin1@gmail.com>
2019-04-02 21:09:55 -0400

    added documentation to map class

--- include/Bloons/Map.hpp
+++ include/Bloons/Map.hpp
@@ -1,30 +1,95 @@
 #pragma once
 
+#include <nlohmann/json.hpp>
 #include <string>
 #include <vector>
 
+#include "Bloons/Loader.hpp"
+#include "Bloons/Manager.hpp"
 #include "Bloons/Path.hpp"
 #include "Bloons/Wave.hpp"
 
 namespace Bloons
 {
 
-class Map
+/**
+ * @brief Stores all map info.
+ * This includes the paths, waves, and the bloon manager itself.
+ * 
+ */
+class Map : public sf::Drawable
 {
 public:
+	/**
+	 * @brief Construct a new Map.
+	 * 
+	 */
 	Map();
 
+	/**
+	 * @brief Add a path to the map.
+	 * 
+	 * @param path The path to add.
+	 */
 	void addPath(Path path);
 
+	/**
+	 * @brief Add a wave to the map.
+	 * 
+	 * @param wave The wave to add.
+	 * 
+	 * @remarks Note the order in which waves are appended, this will 
+	 * be their ID (from 0) to call runWave() with.
+	 */
 	void addWave(Wave wave);
 
+	/**
+	 * @brief Run the wave with the given id.
+	 * 
+	 * @param id The index in which it was added with addWave(). 
+	 */
 	void runWave(size_t id);
 
+	/**
+	 * @brief Call once per frame.
+	 * Updates bloon movements through the bloon manager.
+	 * 
+	 */
 	void update();
 
+	/**
+	 * @brief Initialze the map from the given map config json file.
+	 * 
+	 * @param name The name of the config file.
+	 */
+	void init(std::string name);
+
 private:
+	/**
+	 * @brief SFML's draw() override.
+	 * 
+	 * @remarks Just draws the bloonmanager.
+	 * 
+	 */
+	virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const;
+
+	/**
+	 * @brief The map paths.
+	 * 
+	 */
 	std::vector<Path> mPaths;
+
+	/**
+	 * @brief The map waves.
+	 * 
+	 */
 	std::vector<Wave> mWaves;
+
+	/**
+	 * @brief The map bloon manager.
+	 * 
+	 */
+	Manager mBloonManager;
 };
 
 }
