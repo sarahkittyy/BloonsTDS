04869553dc9963a53267e7027e81316a4e894bfa
sarahkittyy <chrisohlin1@gmail.com>
2019-04-12 11:12:27 -0400

    implemented tower button GUI

---
+++ include/GUI/Towers.hpp
@@ -0,0 +1,128 @@
+#pragma once
+
+#include <imgui/imgui.h>
+#include <SFML/Graphics.hpp>
+
+#include <filesystem>
+#include <fstream>
+#include <string>
+#include <vector>
+
+#include <nlohmann/json.hpp>
+
+#include "ResourceManager.hpp"
+
+namespace GUI
+{
+
+/**
+ * @brief A Tower GUI element, containing information
+ * on texture, name, & description, and any other info.
+ * 
+ */
+class Tower
+{
+public:
+	/**
+	 * @brief Construct a new Tower.
+	 * 
+	 * @param r The application resource manager.
+	 * @param name The name of the tower to load.
+	 * @param size The size of the tower once loaded.
+	 */
+	Tower(ResourceManager* r,
+		  std::string name,
+		  sf::Vector2f size);
+
+	/**
+	 * @brief Get the Sprite.
+	 * 
+	 * @return sf::Sprite The sprite of the tower.
+	 */
+	sf::Sprite getSprite();
+
+
+
+private:
+	/**
+	 * @brief The tower's name.
+	 * 
+	 */
+	std::string name;
+
+	/**
+	 * @brief The tower's description.
+	 * 
+	 */
+	std::string desc;
+
+	/**
+	 * @brief The tower's texture.
+	 * 
+	 */
+	sf::Texture* tex;
+
+	/**
+	 * @brief The sprite, initialized from the tower's texture.
+	 * 
+	 */
+	sf::Sprite spr;
+
+	/**
+	 * @brief Loads the sprite.
+	 * 
+	 * @param tower The tower's json information.
+	 * @param size The size of the tower, once loaded.
+	 */
+	void loadSprite(nlohmann::json tower, sf::Vector2f size);
+
+	/**
+	 * @brief The pointer to the application resource manager.
+	 * 
+	 */
+	ResourceManager* mResources;
+};
+
+/**
+ * @brief Loads the towers, storing them so that they don't have
+ * to be re-read every call.
+ * 
+ */
+class TowerLoader
+{
+public:
+	/**
+	 * @brief Construct a new Tower Loader.
+	 * 
+	 * @param r The application resource manager. 
+	 * @param size The size of each tower sprite to load.
+	 * 
+	 * @remarks A size of -1, -1 indicates use the default size in the texture itself.
+	 */
+	TowerLoader(ResourceManager* r, sf::Vector2f size = {-1, -1});
+
+	/**
+	 * @brief Get a vector of all available game towers.
+	 * 
+	 * @param r Pointer the the application's resource manager.
+	 * 
+	 * @return std::vector<Tower> A vector of all in-game tower options.
+	 */
+	std::vector<Tower>& getGuiTowers();
+
+private:
+	/**
+	 * @brief The loaded towers.
+	 * Loaded through the constructor.
+	 * 
+	 */
+	std::vector<Tower> mTowers;
+
+	/**
+	 * @brief The pointer to the application resource manager.
+	 * 
+	 */
+	ResourceManager* mResources;
+};
+
+}
---
+++ src/GUI/Towers.cpp
@@ -0,0 +1,97 @@
+#include "GUI/Towers.hpp"
+
+namespace GUI
+{
+
+Tower::Tower(ResourceManager* r,
+			 std::string name,
+			 sf::Vector2f size)
+	: mResources(r)
+{
+	using nlohmann::json;
+
+	//Load the tower's info to json
+	std::ifstream file("resource/towers/" + name + ".json");
+	json tower;
+	file >> tower;
+	file.close();
+	//
+
+	//Get the tower info
+	json info = tower["info"];
+	name	  = info["name"];
+	desc	  = info["desc"];
+
+	//load the texture/sprite.
+	loadSprite(tower, size);
+}
+
+void Tower::loadSprite(nlohmann::json tower, sf::Vector2f size)
+{
+	using nlohmann::json;
+
+	//Path to the texture.
+	std::string path = "resource/towers/" + tower["sprite"].get<std::string>();
+
+	//Load the texture.
+	tex = &mResources->texture(path);
+
+	//Initialize the sprite.
+	spr.setTexture(*tex, true);
+
+	//We will use the first frame of the sprite map as the icon
+	sf::IntRect bounds{
+		0, 0,
+		(int)tex->getSize().x / tower["mapsize"][0].get<int>(),
+		(int)tex->getSize().y / tower["mapsize"][1].get<int>()};
+	spr.setTextureRect(bounds);
+
+	//If the size is valid
+	if (size.x != -1 && size.y != -1)
+	{
+		//Set the sprite scaling.
+		spr.setScale(size.x / spr.getGlobalBounds().width,
+					 size.y / spr.getGlobalBounds().height);
+	}
+}
+
+sf::Sprite Tower::getSprite()
+{
+	return spr;
+}
+
+////////////////////////////////////////////
+//                                        //
+// 				TOWER LOADER              //
+//										  //
+////////////////////////////////////////////
+
+TowerLoader::TowerLoader(ResourceManager* r, sf::Vector2f size)
+	: mResources(r)
+{
+	namespace fs = std::filesystem;
+
+	//Iterate over all items in directory resource/towers
+	for (auto& item : fs::directory_iterator("resource/towers"))
+	{
+		//If the extension is .json...
+		std::string ext = item.path().extension().string();
+		if (ext == ".json")
+		{
+			//Get the stem of the file
+			//(a.k.a dart-monkey.json -> dart-monkey)
+			std::string name = item.path().stem().string();
+
+			//Load the tower and push it into the mTowers
+			//vector.
+			mTowers.emplace_back(r, name, size);
+		}
+	}
+}
+
+std::vector<Tower>& TowerLoader::getGuiTowers()
+{
+	return mTowers;
+}
+
+}
