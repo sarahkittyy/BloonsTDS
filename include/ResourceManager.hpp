b973cbf285f587c005243c429d3a98e8f68f1e31
sarahkittyy <chrisohlin1@gmail.com>
2019-04-12 15:03:45 -0400

    added window reference

--- include/ResourceManager.hpp
+++ include/ResourceManager.hpp
@@ -14,6 +14,20 @@ class ResourceManager
 {
 public:
 	/**
+	 * @brief Construct a new Resource Manager
+	 * 
+	 * @param window A pointer to the application window.
+	 */
+	ResourceManager(sf::RenderWindow* window);
+
+	/**
+	 * @brief Get a const reference to the SFML renderwindow.
+	 * 
+	 * @return const sf::RenderWindow& const reference to the window.
+	 */
+	const sf::RenderWindow& getWindow();
+
+	/**
 	 * @brief Retrieve the texture at the given path.
 	 * 
 	 * @param path The path of the texture.
@@ -27,4 +41,10 @@ private:
 	 * 
 	 */
 	std::unordered_map<std::string, sf::Texture> mTextures;
+
+	/**
+	 * @brief Pointer to the SFML window.
+	 * 
+	 */
+	sf::RenderWindow* mWindow;
 };
--- src/ResourceManager.cpp
+++ src/ResourceManager.cpp
@@ -1,5 +1,15 @@
 #include "ResourceManager.hpp"
 
+ResourceManager::ResourceManager(sf::RenderWindow* window)
+	: mWindow(window)
+{
+}
+
+const sf::RenderWindow& ResourceManager::getWindow()
+{
+	return *mWindow;
+}
+
 sf::Texture& ResourceManager::texture(std::string path)
 {
 	auto found = mTextures.find(path);
